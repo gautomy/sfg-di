981e68f8b5168800318629f306adc85149b2d4a6
Tomy <gautomy@gmail.com>
2023-01-01 22:16:40 +0100

    constructor dependency injection

---
+++ src/main/java/gautomy/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,16 @@
+package gautomy.springframework.sfgdi.controllers;
+
+import gautomy.springframework.sfgdi.services.GreetingService;
+
+public class ConstructorInjectedController {
+
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}
