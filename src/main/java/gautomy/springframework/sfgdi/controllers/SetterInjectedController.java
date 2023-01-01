a72ed5e411ea9d9ece352d833aeb4e6b468adae3
Tomy <gautomy@gmail.com>
2023-01-01 22:08:52 +0100

    setter dependency injection

---
+++ src/main/java/gautomy/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -0,0 +1,17 @@
+package gautomy.springframework.sfgdi.controllers;
+
+import gautomy.springframework.sfgdi.services.GreetingService;
+
+public class SetterInjectedController {
+
+    private GreetingService greetingService;
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+
+}
